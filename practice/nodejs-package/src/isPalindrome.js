4ba1cea9631c58a10ad84edc134dd2193da8c518
Dmytro Pavlenko <atosdo@live.com>
2017-08-21 15:15:31 +0300

    reorganize files

---
+++ practice/nodejs-package/src/isPalindrome.js
@@ -0,0 +1,13 @@
+const isPalindrome = str => {
+    const length = str.length;
+
+    if (length === 1) return true;
+
+    if (str[0] === str[length - 1]) {
+        return isPalindrome(str.substring(1, length - 1));
+    } else {
+        return false;
+    }
+};
+
+export default isPalindrome;
