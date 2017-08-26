313da6a47350ca5613699534383e56036b3555d2
Dmytro P <atosdo@live.com>
2017-08-26 22:02:51 +0300

    add solved points exercise

--- compound_data/nodejs-package/__tests__/point.test.js
+++ compound_data/nodejs-package/__tests__/point.test.js
@@ -1,20 +1,17 @@
+import { makePoint, toString } from 'hexlet-points';
 import * as point from '../src/point';
 
 test('point', () => {
-  expect(point.getX(point.makePoint(1, 2))).toBe(1);
-  expect(point.getY(point.makePoint(1, 2))).toBe(2);
-  expect(point.toString(point.makePoint(1, 2))).toBe('(1, 2)');
+  expect(point.distance(makePoint(0, 0), makePoint(3, 4))).toBe(5);
+  expect(point.distance(makePoint(0, 0), makePoint(0, 0))).toBe(0);
 
-  expect(point.distance(point.makePoint(0, 0), point.makePoint(3, 4))).toBe(5);
-  expect(point.distance(point.makePoint(0, 0), point.makePoint(0, 0))).toBe(0);
+  expect(point.quadrant(makePoint(5, 3))).toBe(1);
+  expect(point.quadrant(makePoint(-5, 3))).toBe(2);
+  expect(point.quadrant(makePoint(-2, -10))).toBe(3);
+  expect(point.quadrant(makePoint(7, -9))).toBe(4);
+  expect(point.quadrant(makePoint(0, 0))).toBe(-1);
 
-  expect(point.quadrant(point.makePoint(5, 3))).toBe(1);
-  expect(point.quadrant(point.makePoint(-5, 3))).toBe(2);
-  expect(point.quadrant(point.makePoint(-2, -10))).toBe(3);
-  expect(point.quadrant(point.makePoint(7, -9))).toBe(4);
-  expect(point.quadrant(point.makePoint(0, 0))).toBe(-1);
-  
-  expect(point.toString(point.symmetricalPoint(point.makePoint(0, 0)))).toBe('(0, 0)');
-  expect(point.toString(point.symmetricalPoint(point.makePoint(2, 4)))).toBe('(-2, -4)');
-  expect(point.toString(point.symmetricalPoint(point.makePoint(7, -3)))).toBe('(-7, 3)');
+  expect(toString(point.symmetricalPoint(makePoint(0, 0)))).toBe('(0, 0)');
+  expect(toString(point.symmetricalPoint(makePoint(2, 4)))).toBe('(-2, -4)');
+  expect(toString(point.symmetricalPoint(makePoint(7, -3)))).toBe('(-7, 3)');
 });
--- compound_data/nodejs-package/src/point.js
+++ compound_data/nodejs-package/src/point.js
@@ -1,30 +1,19 @@
-export const makePoint = (x, y) => m => m(x, y);
-export const getX = point => point((x, y) => x);
-export const getY = point => point((x, y) => y);
-export const toString = point => `(${getX(point)}, ${getY(point)})`;
-export const distance = (p1, p2) => Math.sqrt((getX(p2) - getX(p1)) ** 2 + (getY(p2) - getY(p1)) ** 2);
+import { makePoint, getX, getY } from 'hexlet-points';
 
-export const quadrant = point => {
-    const x = getX(point);
-    const y = getY(point);
+export const quadrant = (point) => {
+  const x = getX(point);
+  const y = getY(point);
 
-    if (x > 0 && y > 0) {
-        return 1;
-    }
+  if (x > 0 && y > 0) return 1;
+  if (x < 0 && y > 0) return 2;
+  if (x < 0 && y < 0) return 3;
+  if (x > 0 && y < 0) return 4;
 
-    if (x < 0 && y > 0) {
-        return 2;
-    }
-
-    if (x < 0 && y < 0) {
-        return 3;
-    }
-
-    if (x > 0 && y < 0) {
-        return 4;
-    }
-
-    return -1;
+  return -1;
 };
 
-export const symmetricalPoint = point => makePoint(-getX(point), -getY(point));
+export const distance = (point1, point2) =>
+  Math.sqrt((getX(point2) - getX(point1)) ** 2 + (getY(point2) - getY(point1)) ** 2);
+
+export const symmetricalPoint = point =>
+  makePoint(-getX(point), -getY(point));
