a88c6182202e82b42f4b38e10ecbe9d1d0d3e4ad
Dmytro P <atosdo@live.com>
2017-08-27 22:20:29 +0300

    modify some test

--- practice/nodejs-package/__tests__/CompoundData/pairsWithoutFunctions.test.js
+++ practice/nodejs-package/__tests__/CompoundData/pairsWithoutFunctions.test.js
@@ -4,6 +4,6 @@ test('car, cdr', () => {
   expect(car(cons(1, 2))).toBe(1);
   expect(cdr(cons(1, 2))).toBe(2);
 
-  expect(car(cons(-1, 125))).toBe(-1);
-  expect(cdr(cons(489, -2582))).toBe(-2582);
+  expect(car(cons(15, 11))).toBe(15);
+  expect(cdr(cons(15, 11))).toBe(11);
 });
